af605949404913d035ab31622f0289a6ab70a06c
jerans16 <jeanpaoloramos@gmail.com>
2021-07-12 09:47:27 -0400

    Login hecho

--- ProyectoWatchYou/apps/master/views.py
+++ ProyectoWatchYou/apps/master/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from apps.usuarios.models import Usuario
 
 # Create your views here.
@@ -7,7 +7,16 @@ def index(request):
     if request.method == "GET":
         return render(request,'index.html')
     elif request.method == "POST":
-        pass
+        try:
+            usuario = Usuario.objects.get(email=request.POST['email'])
+        except:
+            usuario = None
+        if usuario and usuario.password == request.POST['pw']:
+            request.session['id'] = usuario.id
+            request.session['usuario'] = usuario.email
+            return redirect("/dashboard")
+        else:
+            return redirect("/")
 
 def dashboard(request):
     return render(request, 'dashboard.html')
