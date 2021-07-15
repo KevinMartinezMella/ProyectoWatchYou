d73e591a8ce99426c7aca583db1e8bf25d478b16
jerans16 <jeanpaoloramos@gmail.com>
2021-07-15 00:32:37 -0400

    avances

--- ProyectoWatchYou/apps/clases/monitor.py
+++ ProyectoWatchYou/apps/clases/monitor.py
@@ -2,32 +2,46 @@ import pyttsx3
 import platform
 import subprocess
 import os as op
-from send_mail import Email
+from apps.clases.send_mail import Email
+#from apps.clases.send_wsp import Msg
 from datetime import datetime
 from pyttsx3 import engine
-#from apps.servidores.models import Servidor
+from apps.servidores.models import Servidor
 
 
 class Monitor:
 	def __init__(self):
-		#self.host = Servidor.objects.all()
-		self.host = ['www.google.com']
+		self.host = Servidor.objects.all()
+	# 	self.idserver = ""
+	# 	#self.host = ['www.google.com']
 	
-	def ping(self):
-		for i in range(0,len(self.host)):
+	def ping(self,idserver=""):
+		if len(idserver) > 0 or idserver != "":
+			self.host = Servidor.objects.get(id=idserver)
 			os = "-n" if platform.system().lower() == "windows" else "-c"
-			action = ['ping', os,'5', self.host[i]]
+			action = ['ping', os,'5', self.host.ip]
 			subprocess.call(action)
-			p = subprocess.Popen('ping '+self.host[i],stdout=subprocess.PIPE)
+			p = subprocess.Popen('ping '+self.host.ip,stdout=subprocess.PIPE)
 			p.wait()
-		if p.poll():
-			print(self.host[i]+" is Down")
-			stat = False
+			global nombre
+			nombre = self.host.nombre_servidor
 		else:
-			print(self.host[i]+" is Up")
-			stat = True
+			self.host = Servidor.objects.all()
+			for i in self.host:
+				os = "-n" if platform.system().lower() == "windows" else "-c"
+				action = ['ping', os,'5', i.ip]
+				subprocess.call(action)
+				p = subprocess.Popen('ping '+i.ip,stdout=subprocess.PIPE)
+				p.wait() 
+				nombre = i.nombre_servidor
+				if p.poll():
+					print(i.nombre_servidor+" is Down")
+					stat = False
+				else:
+					print(i.nombre_servidor+" is Up")
+					stat = True
 
-		return stat
+			return stat
 	
 ejecutar = Monitor()
 ejecutar.ping()
@@ -39,14 +53,14 @@ class Validar:
 		self.engine.setProperty('rate',300)
 
 	def verificar(self):
-		data = Monitor()
-		self.engine.setProperty('rate',300)
 		if ejecutar:
 			print("Equipos operativos")
-			self.engine.say("Funciona")
-			self.engine.runAndWait()
 			new_email = Email()
-			new_email.sendMail(data.host)
+			new_email.sendMail(nombre+" Is Up")
+			# new_message = Msg()
+			# new_message.mensaje(nombre)
+			# self.engine.say("Funciona")
+			# self.engine.runAndWait()
 
 		else:
 			print("Equipo Caido")
