2fa7c561888e53e8f2070b4568b5a0b545a68ea0
jerans16 <jeanpaoloramos@gmail.com>
2021-07-19 11:32:44 -0400

    ayuda David

--- ProyectoWatchYou/apps/master/views.py
+++ ProyectoWatchYou/apps/master/views.py
@@ -7,7 +7,8 @@ from apps.clases.monitor import Monitor, Validar
 from apps.schedules.models import *
 from apps.schedules.views import agregarSchedule
 from datetime import datetime, timezone
-import schedules
+import threading
+import schedule
 import time
 
 
@@ -77,50 +78,35 @@ def edit(request,idserver):
     data = update(request,idserver)
     return redirect('/devices')
 
-def programar(request):
-    idserver = request.POST['select']
-    idhora = request.POST['hora']
-    agregarSchedule(request, idserver, idhora)
-    hoy = datetime.now()
-    agenda = Schedule.objects.get(id = idserver)
-    time = "%2d:%02d" %(hoy.hour, hoy.minute)
-    print(idhora)
-    schedules.every().day.at("idhora").do(probar(request))
-    while True:
-        try:
-            schedules.run_pending()
-            time.sleep(1)
-        except:
-            continue
-        stop = input('Presiona q para finalizar el programa')
-        if stop=='q':
-            break
-    # print("%2d:%02d" %(hoy.hour, hoy.minute))
-        # probar(idserver = idserver)
-
-    # return redirect("/devices")
 
 def probar(request):
-    # idserver = request.POST['select']
-    idserver = "1"
+    idserver = request.POST['select']
     monitor = Monitor()
     isUp = monitor.ping(idserver)
     validar = Validar()
     validar.verificar(isUp)
     return redirect("/devices")
 
+def hola():
+    print("HOLAAAAAAAAAAAAAAA")
+
+def programar(request):
+    idserver = request.POST['select']
+    idhora = request.POST['hora']
+    agregarSchedule(request, idserver, idhora)
+    hoy = datetime.now()
+    agenda = Schedule.objects.filter(id = idserver)
+    time = "%2d:%02d" %(hoy.hour, hoy.minute)
+    print(idhora)
+    now = datetime.now()
+    run = datetime(2021, 7, 19, 11, 11)
+    delay = (run - now).total_seconds()
+    task = threading.Timer(delay, probar)
+    task.start()
+
+    # return redirect("/devices")
+
 def cerrar(request):
     del request.session['usuario']
     del request.session['id']
     return redirect("/")
-
-# def probandoPrueba(request):
-#     probar()
-#     return HttpResponse("funciona")
-
-
-# schedule.every().day.at("14:44").do(cerrar(request))
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
