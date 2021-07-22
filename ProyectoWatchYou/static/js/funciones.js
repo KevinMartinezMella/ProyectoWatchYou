3b7f998c46390b0f2455bb9b7db9077a8400c6c1
KevinMartinezMella <iamkamm7@gmail.com>
2021-07-21 23:00:40 -0400

    cambio de imagen

--- ProyectoWatchYou/static/js/funciones.js
+++ ProyectoWatchYou/static/js/funciones.js
@@ -87,3 +87,61 @@ function crearGrafico(estados){
 //     var request = new Request('/datos',{method: 'GET'});
 //     return request     
 // }
+
+
+// window.onload = function(){
+//     var contenedor = document.getElementById('contenedor_carga')
+//     contenedor.style.visibility = 'hidden';
+//     contenedor.style.opacity = '0';
+
+// }
+
+function carga(){
+setTimeout(function(){
+    var contenedor = document.getElementById('contenedor_carga');
+
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+
+    }, 3000)
+}
+
+
+
+
+function video(){
+(function () {
+
+    var bv = new Bideo();
+    bv.init({
+      // Video element
+      videoEl: document.querySelector('#background_video'),
+  
+      // Container element
+      container: document.querySelector('body'),
+  
+      // Resize
+      resize: true,
+  
+      // autoplay: false,
+  
+      // Array of objects containing the src and type
+      // of different video formats to add
+      src: [
+        {
+          src: 'https://vjs.zencdn.net/v/oceans.mp4',
+          type: 'video/mp4'
+        },
+        {
+          src: 'https://vjs.zencdn.net/v/oceans.webm',
+          type: 'video/webm;codecs="vp8, vorbis"'
+        }
+      ],
+  
+      // What to do once video loads (initial frame)
+      onLoad: function () {
+        document.querySelector('#video_cover').style.display = 'none';
+      }
+    });
+  }());
+} 
