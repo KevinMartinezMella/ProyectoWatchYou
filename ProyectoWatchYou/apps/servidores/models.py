5038486895c322650a4fc11f85931235afba0262
jerans16 <jeanpaoloramos@gmail.com>
2021-07-23 02:43:36 -0400

    x

--- ProyectoWatchYou/apps/servidores/models.py
+++ ProyectoWatchYou/apps/servidores/models.py
@@ -8,6 +8,6 @@ class Servidor(models.Model):
     ip = models.CharField(max_length=255,null=False,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    usuario = models.ManyToManyField(Usuario, related_name='servidores')
     def __str__(self):
         return f'{self.ip}'
