4b53b3b4786ec2c4cd2573abef9e4a7a366201d1
jerans16 <jeanpaoloramos@gmail.com>
2021-07-23 00:55:28 -0400

    terminado

--- ProyectoWatchYou/apps/estados/models.py
+++ ProyectoWatchYou/apps/estados/models.py
@@ -1,6 +1,7 @@
 from os import name
 from django.db import models
 from apps.servidores.models import Servidor
+from apps.usuarios.models import Usuario
 
 # Create your models here.
 class Estado(models.Model):
@@ -15,6 +16,6 @@ class EstadoServidor(models.Model):
     fechahora = models.DateTimeField(auto_now_add=True)
     servidores = models.ForeignKey(Servidor, related_name="estados", on_delete=models.CASCADE)
     estados = models.ForeignKey(Estado, related_name="servidores", on_delete=models.CASCADE)
-
+    
     def __repr__(self):
         return self.estados.estado
